f956392e298feae9b7a94e17806a8f5ac2bbf24d
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-28 21:19:42 +0000

    improved responsiveness and created dark/light mode toggle

--- scipt.js
+++ scipt.js
@@ -1,8 +1,17 @@
 // Variables
 let menu = document.querySelector(".jsMenu");
 let navList = document.querySelector(".jsNavList");
+let displayModeBtn = document.querySelector(".jsDisplayMode");
+let displayModeIcon = document.querySelector(".jsDisplayModeIcon");
 
 // Show nav menu on menu button click
 menu.addEventListener("click", () => {
   navList.classList.toggle("show-nav");
+});
+
+// Theme icon toggles/primary and Secondary colors switch when dark theme is toggled
+displayModeBtn.addEventListener("click", () => {
+  displayModeIcon.classList.toggle("fa-sun");
+  displayModeIcon.classList.toggle("fa-moon");
+  document.body.classList.toggle("dark-theme");
 });
